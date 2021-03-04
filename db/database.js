bb1abff25085965ef3a79f9777a0c564f3d2d322
rolanduwxcc <rolanduwxcc@gmail.com>
2021-03-03 22:56:57 -0600

    created database.js and routes/apiroutes

--- db/database.js
+++ db/database.js
@@ -1,9 +1,8 @@
 const { moduleExpression } = require('@babel/types');
-
 const sqlite3 = require('sqlite3').verbose();
 
 //connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
+const db = new sqlite3.Database('db/election.db', err => {
     if (err) {
         return console.error(err.message);
     }
